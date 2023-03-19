1aae7d5393065e1790efc9df17aa29288433e88c
Nguyễn Văn Hoàng Nhân <57064711+cyantiz@users.noreply.github.com>
2023-03-19 22:56:30 +0700

    sync!: merge branch 'develop' to main (#9)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,6 @@ module.exports = {
                 'almost-white': '#FEFEFE',
                 inactive: '#687079',
                 'inactive-base': '#F8FAFB',
-
             },
         },
     },
